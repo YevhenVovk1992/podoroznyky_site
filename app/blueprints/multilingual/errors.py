4dfb901f8cdaf69d08e58b6013128f7151819e7c
VovkWorkPC <vovk.valki@gmail.com>
2023-07-11 11:56:15 +0300

    Create errors handler
    add 404 handler;
    add 500 handler;

--- app/blueprints/multilingual/errors.py
+++ app/blueprints/multilingual/errors.py
@@ -4,9 +4,9 @@ from app import app
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('multilingual/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'), 500
+    return render_template('multilingual/500.html'), 500
