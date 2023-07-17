34f5c913dc27ef40e18541c0040eb6bf428473d4
VovkWorkPC <vovk.valki@gmail.com>
2023-07-17 13:53:49 +0300

    Display all images in a folder. Added the ability to manually translate photos.

--- app/blueprints/multilingual/routes.py
+++ app/blueprints/multilingual/routes.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from flask import request, render_template, Blueprint, g, abort, current_app
 
 
@@ -41,5 +44,28 @@ def contact_view():
 def photo_view():
     """
     The function renders page with images.
+    All images are stored in '../app/static/img/' link.
     """
-    return render_template('multilingual/photos.html', title='Photos')
+
+    # Search all images in a folder. Group them into 3 groups.
+    comment_text = None
+    base_dir = os.path.abspath(os.curdir)
+    land_code = g.lang_code
+    images_list = os.listdir(base_dir + '/app/static/img/photos/')
+    n = len(images_list)//3
+    list_for_template = [images_list[i:i + n] for i in range(0, len(images_list), n)]
+
+    # Reading signatures for images from a file. Choice of translation depending on the user's language.
+    with open(base_dir + '/app/img_text.json', 'r', encoding='utf-8') as file:
+        json_text = file.read()
+    multilingual_text = json.loads(json_text)
+    if land_code == 'en':
+        comment_text = multilingual_text.get('en')
+    elif land_code == 'uk':
+        comment_text = multilingual_text.get('uk')
+
+    return render_template(
+        'multilingual/photos.html',
+        title='Photos',
+        content={'images_list': list_for_template, 'comment_text': comment_text}
+    )
